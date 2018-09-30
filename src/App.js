04bf8e4c6e7b723a6e62becf8c59978e6bec9d11
Alessandro Pepe <apepe1024@gmail.com>
2018-09-30 13:28:55 -0400

    I tried to utilize an API but I didn't do it right. Enough for today.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,55 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import scriptLoader from 'react-async-script-loader';
+
 
 class App extends Component {
+
+  constructor(props) {
+      super(props);
+      this.state = {
+          'coordinates': ''
+      };
+      // binding 'this' instances
+      this.setCoordinates = this.setCoordinates.bind(this);
+  }
+
+  componentDidMount() {
+    window.setCoordinates = this.setCoordinates;
+  }
+
+  setCoordinates() {
+      fetch("http://api.open-notify.org/iss-now.json")
+          .then(
+              function (response) {
+                if (response.status !== 200) { throw response.err; }
+                  response.json().then(function (data) {
+                      let latitude = data.response.iss_position.latitude;
+                      let longitude = data.response.iss_position.longitude;
+                      let formattedPosition = "<p> lat: " + latitude + ", lng: " + longitude + "</p>";
+                      this.setState({
+                        'coordinates': formattedPosition
+                      });
+                  })
+              }
+
+          )
+  }
+
   render() {
+
+    const renderCoordinates = this.state.coordinates;
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        <p>Test</p>
+        <p>
+          {renderCoordinates}
         </p>
       </div>
     );
